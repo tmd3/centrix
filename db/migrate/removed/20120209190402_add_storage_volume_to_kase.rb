b4d2c8770dfe3f5fde9c4fff818c30324d7ff655
tmd <thomasmdailey3@gmail.com>
2012-02-09 16:29:18 -0800

    Rename storage volume migration file -- 2nd try.

---
+++ db/migrate/removed/20120209190402_add_storage_volume_to_kase.rb
@@ -0,0 +1,7 @@
+class AddStorageVolumeToKase < ActiveRecord::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
---
+++ db/migrate/removed/20120209191452_add_storage_volume_to_kase.rb
@@ -0,0 +1,7 @@
+class AddStorageVolumeToKase < ActiveRecord::Migration
+
+  def change
+    add_column :products, :part_number, :string
+  end
+end
+
